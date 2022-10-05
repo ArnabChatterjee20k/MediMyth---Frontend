f79f18211548d775c32d2da5691c23d9b0d85ccd
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-10-05 13:39:51 +0530

    Footer Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import Tile from "./components/Category/Tile"
+import { PrimaryButton } from "./components/ui/Buttons"
+import Navbar from "./layouts/Navbar"
+import Footer from "./layouts/Footer"
+import { PrimaryLogo } from "./components/Logo/PrimaryLogo"
+import { SecondaryLogo } from "./components/Logo/SecondaryLogo"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Hellow World
+      <Footer/>
     </div>
   )
 }
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography  from '@mui/material/Typography'
+import Stack  from '@mui/material/Stack'
+const Footer = () => {
+  return (
+    <Stack direction="row" justifyContent="center">
+        <Typography fontWeight={400} fontSize="2rem" lineHeight="44px">
+            MediMyth @ 2022 ALL RIGHTS RESERVED
+        </Typography>
+    </Stack>
+  )
+}
+
+export default Footer
