bf60b08a8270ca26787ca50f789ea5aec0fa8b60
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-10-14 23:03:29 +0530

    added query client and dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import {queryClient} from "./queryClient/queryClient"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
