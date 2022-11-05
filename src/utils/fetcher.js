3b58207726af1729a6e8b46739563e627c246f87
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-11-05 11:50:57 +0530

    added options option in fetcher for adding other options

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,5 +1,5 @@
-export function getFetcher(url){
-    return fetch(url).then(data=>{
+export function getFetcher(url,options=null){
+    return fetch(url,options).then(data=>{
         if(data.status === 200) return data.json()
         if(data.status === 404) throw {status:"redirect"}
         throw {status:"error"}
