7c497502c873e7eb5a4f882ce85f7f83549cdf83
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-11-05 10:25:18 +0530

    increased the stale time in query client

--- src/queryClient/queryClient.jsx
+++ src/queryClient/queryClient.jsx
@@ -1,5 +1,6 @@
 import { QueryClient } from '@tanstack/react-query'
 
 export const queryClient = new QueryClient({defaultOptions:{queries:{
-    staleTime:4000,retry:1    
+    staleTime:1800000, // 30 mins
+    retry:1    
 }}})
