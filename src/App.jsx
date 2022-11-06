89069a5b63216203f349c34e1eed752dca14ce4d
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-11-06 13:17:52 +0530

    Added suspense loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
+import { lazy , Suspense } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter } from "react-router-dom";
 import DoctorRoutes from "./routes/DoctorRoutes";
-import { MultiStepDoctorRegistration } from "./features/doctor-multistep-register";
 import NotificationContextProvider from "./contexts/ToastContextProvider/NotificationContextProvider";
-import Toast from "./components/ui/Toast";
 import DoctorProfileContextProvider from "./contexts/DoctorProfileContextProvider/DoctorProfileContextProvider";
+import Stack  from "@mui/material/Stack";
+import SuspenseLoader from "./components/ui/SuspenseLoader";
+
+const Navbar = lazy(()=>import("./layouts/Navbar"))
+const Toast = lazy(()=>import("./components/ui/Toast"))
+
 function App() {
   return (
     <NotificationContextProvider>
       <DoctorProfileContextProvider>
         <CssBaseline />
-        <BrowserRouter>
-          <DoctorRoutes />
-        </BrowserRouter>
-        <Toast />
+        <Suspense fallback={<SuspenseLoader/>}>
+        <Navbar/>
+        <Stack paddingTop={10}>
+          <BrowserRouter>
+            <DoctorRoutes />
+          </BrowserRouter>
+          <Toast />
+        </Stack>
+        </Suspense>
         {/* <MultiStepDoctorRegistration /> */}
         {/* <ScheduleCalendar/> */}
         {/* <AppointmentViewer/> */}
---
+++ src/components/ui/SuspenseLoader.jsx
@@ -0,0 +1,14 @@
+import {Dna} from "react-loader-spinner"
+
+const SuspenseLoader = () => {
+  return (
+    <Dna
+    height = "80"
+    width = "80"
+    radius = "9"
+    color = 'green'
+  />
+  )
+}
+
+export default SuspenseLoader
