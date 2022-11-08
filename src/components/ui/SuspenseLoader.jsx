4b717ea025b207c187be153c73d0f7137a83a6e0
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-11-08 16:59:52 +0530

    Made the Suspense Loader centered

--- src/components/ui/SuspenseLoader.jsx
+++ src/components/ui/SuspenseLoader.jsx
@@ -1,13 +1,17 @@
 import {Dna} from "react-loader-spinner"
-
+import Stack  from "@mui/material/Stack"
 const SuspenseLoader = () => {
   return (
-    <Dna
-    height = "80"
-    width = "80"
-    radius = "9"
-    color = 'green'
-  />
+    <Stack alignItems="center" justifyContent="center" sx={{
+      width:"100%",minHeight:"100vh"
+    }}>
+      <Dna
+      height = "80"
+      width = "80"
+      radius = "9"
+      color = 'green'
+    />
+    </Stack>
   )
 }
 
