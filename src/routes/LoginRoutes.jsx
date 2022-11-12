6bee419633d3a6d603702ed71ea04febdfc45968
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-11-12 19:08:08 +0530

    added redirect logic and saving token logic

--- src/routes/LoginRoutes.jsx
+++ src/routes/LoginRoutes.jsx
@@ -1,21 +1,32 @@
-import {lazy} from 'react'
-import {Routes,Route} from "react-router-dom"
-const DoctorRegistration = lazy(()=>import('../features/doctor-multistep-register/index'))
-const DoctorLogin = lazy(()=>import('../features/login/index'))
-const Auth = lazy(()=>import("../components/Auth/AuthCard"))
-import { DoctorAccountActions } from '../data/DoctorAccountActions'
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+const DoctorRegistration = lazy(() =>
+  import("../features/doctor-multistep-register/index")
+);
+const DoctorLogin = lazy(() => import("../features/login/index"));
+const Auth = lazy(() => import("../components/Auth/AuthCard"));
+import { DoctorAccountActions } from "../data/DoctorAccountActions";
+import { useIsAuthenticated } from "react-auth-kit";
+import { Navigate } from "react-router-dom";
 const LoginRoutes = () => {
+  // it will work for either doctor or patient.
+  // so while building it for patients just see the patient or doctor in the url and redirects them accordingly
+  const isLogged = useIsAuthenticated()
+  const redirect = isLogged()
+  if(redirect){
+    return <Navigate to="/doctor/profile"/>
+  }
   return (
     <Routes>
-        <Route path='/account'>
-            <Route path='doctor'>
-              <Route path=''  element={<Auth links={DoctorAccountActions}/>}/>
-              <Route path='register'element={<DoctorRegistration/>}/>
-              <Route path='login'element={<DoctorLogin/>}/>
-              </Route>
+      <Route path="/account">
+        <Route path="doctor">
+          <Route path="" element={<Auth links={DoctorAccountActions} />} />
+          <Route path="register" element={<DoctorRegistration />} />
+          <Route path="login" element={<DoctorLogin />} />
         </Route>
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default LoginRoutes
+export default LoginRoutes;
--- src/services/useSaveTokenQuery.jsx
+++ src/services/useSaveTokenQuery.jsx
@@ -4,7 +4,10 @@ export const useSaveTokenQuery = () => {
   // saving to the local storage
   const signIn = useSignIn();
   function saveToStorage({ authToken }) {
-    return signIn({ token: authToken, expiresIn: 3600,authState:{token:authToken}, tokenType: "Bearer" });
+    // in future role can be used to detect whether a doctor or a patient. 
+    // If bychance any one changes doctor to patient then error will come in a doctor route or it will be not found
+    // at that time just log them out and again do the login process
+    return signIn({ token: authToken, expiresIn: 3600,authState:{token:authToken,role:"doctor"}, tokenType: "Bearer" });
   }
   return saveToStorage;
 };
