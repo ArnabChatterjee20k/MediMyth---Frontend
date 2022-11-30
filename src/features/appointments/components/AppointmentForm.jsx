d447ed754545a3dece45806a3f457d1761955640
arnab <arnabchatterjee.ac.2@gmail.com>
2022-11-30 15:18:03 +0530

    added submission functions to thr forms

--- src/features/appointments/components/AppointmentForm.jsx
+++ src/features/appointments/components/AppointmentForm.jsx
@@ -1,17 +1,20 @@
 import Stack from "@mui/material/Stack";
 import { lazy } from "react";
 import MultiStepperFormWrapper from "../../../components/Form/MultiStepperFormWrapper";
+import { useAppointmentFormContext } from "../context/AppointmentFormContextProvider";
 const OTPForm = lazy(() => import("./OTPForm"));
 const PatientDetailsForm = lazy(() => import("./PatientDetailsForm"));
 
 export default function AppointmentForm() {
   const label = "Patient";
+  const {sendOTP,registerPatient} = useAppointmentFormContext();
   const elements = [
-    { Component: PatientDetailsForm, label: label },
+    { Component: PatientDetailsForm, label: label,onSubmit:sendOTP },
     {
       Component: OTPForm,
       onSubmit: () => console.log("sending otp"),
       label: label,
+      onSubmit:registerPatient
     },
   ];
   return (
