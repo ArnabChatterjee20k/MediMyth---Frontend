fd6d35209e1f76c12d5feb7577cd2812e3b34531
arnab <arnabchatterjee.ac.2@gmail.com>
2022-11-30 15:08:11 +0530

    created patient details form

--- src/features/appointments/components/PatientDetailsForm.jsx
+++ src/features/appointments/components/PatientDetailsForm.jsx
@@ -1,19 +1,19 @@
 import Field from "../../../components/Form/Field";
 import { useAppointmentFormContext } from "../context/AppointmentFormContextProvider";
-
+import { PhoneNumber } from "../../../data/validErrorPatterns";
 export default function PatientDetailsForm() {
     const {patientDetails,setPatientDetails} = useAppointmentFormContext()
-    const {name,age,appointment_date,contact_number} = patientDetails
+    const {name,age,contact_number} = patientDetails
     const handleChange = (e)=>{
       setPatientDetails(prevData=>({...prevData,[e.target.name]:e.target.value}))
     }
-    // const inputValue = patientDetails[name];
-    // const validInput = new RegExp(errorPattern).test(inputValue);
+    const inputValue = contact_number;
+    const validInput = new RegExp(PhoneNumber).test(inputValue);
   return (
     <>
         <Field name="name" value={name}  type="text" label="Name" placeholder="Enter Your Name" changeHandler={handleChange} />
-        <Field name="age" value={age}  type="text" label="Age" placeholder="Enter Your Sge" changeHandler={handleChange}/>
-        <Field name="contact_number" value={contact_number}  type="text" label="Phone Number" placeholder="Enter Your Phone Number" changeHandler={handleChange}/>
+        <Field name="age" value={age}  type="text" label="Age" placeholder="Enter Your Age" changeHandler={handleChange}/>
+        <Field name="contact_number" value={contact_number} showError={!validInput} helperText={!validInput && "Enter a valid Phone Number"}  type="text" label="Phone Number" placeholder="Enter Your Phone Number" changeHandler={handleChange}/>
     </>
   )
 }
