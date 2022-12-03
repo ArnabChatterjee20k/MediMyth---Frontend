7b3242a6fb0649918ae138456d9da44e5058b559
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-03 10:12:12 +0530

    fixed the typo

--- src/components/DoctorProfile/DoctorImage.jsx
+++ src/components/DoctorProfile/DoctorImage.jsx
@@ -3,7 +3,7 @@ import { PrimaryTextColor } from "../../assets/styles/DefaultProperties";
 import PropTypes from "prop-types";
 import Avatar  from "@mui/material/Avatar";
 import deepOrange  from "@mui/material/colors/deepOrange";
-const DoctorImage = ({ profilePicture, name ,displayInBlock}) => {
+const DoctorImage = ({ profilePicture, name ,displayInFree}) => {
   return (
     <>
       <Box
@@ -16,9 +16,9 @@ const DoctorImage = ({ profilePicture, name ,displayInBlock}) => {
       >
         <Box
           sx={{
-            position: displayInBlock && "absolute",
-            top: displayInBlock && "-6rem",
-            borderRadius: displayInBlock && "800px",
+            position: displayInFree && "absolute",
+            top: displayInFree && "-6rem",
+            borderRadius: "800px",
             border: `6px solid ${PrimaryTextColor}`,
             backgroundColor: "white",
           }}
@@ -46,13 +46,13 @@ const DoctorImage = ({ profilePicture, name ,displayInBlock}) => {
 DoctorImage.propTypes = {
   profilePicture: PropTypes.string,
   name:PropTypes.string.isRequired,
-  displayInBlock:PropTypes.bool.isRequired
+  displayInFree:PropTypes.bool.isRequired
 };
 
 DoctorImage.defaultProps = {
   profilePicture: "Tile",
   name:"Doctorname",
-  displayInBlock:true
+  displayInFree:true
 };
 
 export default DoctorImage;
