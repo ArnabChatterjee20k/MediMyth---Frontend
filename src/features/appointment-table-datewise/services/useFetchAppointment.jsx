288e79dfbc46531120d4d4f5729931b59a6c5bff
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-05 15:38:40 +0530

    added appointment date as the key in the useQuery in useFetchAppointment as with date change it must fetch

--- src/features/appointment-table-datewise/services/useFetchAppointment.jsx
+++ src/features/appointment-table-datewise/services/useFetchAppointment.jsx
@@ -3,6 +3,6 @@ import fetchAppointment from "../utils/fetchAppointment"
 
 export default function useFetchAppointment(schedule_id,appointment_date) {
   return (
-    useQuery(["Appointments",schedule_id],()=>fetchAppointment(schedule_id,appointment_date))
+    useQuery(["Appointments",schedule_id,appointment_date],()=>fetchAppointment(schedule_id,appointment_date))
   )
 }
