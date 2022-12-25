0027636271cb95ea2c007152e32832436f1a3d62
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-25 11:05:40 +0530

    added the banner at home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,18 @@
-import CategorySearch from "./CategorySearch"
+import { Suspense } from "react";
+import CategorySearch from "./CategorySearch";
+import Stack from "@mui/material/Stack";
+import HomeBanner from "../../assets/img/Banner.png";
 export default function Home() {
   return (
-    <CategorySearch/>
-  )
+    <>
+      <Stack flexDirection="row" sx={{
+        marginInline:"auto",
+        maxWidth:{"xs":"90%",sm:"80%"},
+        marginBottom:5
+      }}>
+        <img src={HomeBanner} alt="" loading="lazy" style={{maxWidth:"100%"}}/>
+      </Stack>
+      <CategorySearch />
+    </>
+  );
 }
