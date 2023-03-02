15b1c5c6a0c3cca633f82bec27a52e2a8e20445b
arnab <arnabchatterjee.ac.2@gmail.com>
2023-03-02 09:04:50 +0530

    Made the environment configs

--- src/features/searchengine/components/DoctorSearch.jsx
+++ src/features/searchengine/components/DoctorSearch.jsx
@@ -5,9 +5,10 @@ import Stack from "@mui/material/Stack";
 import CardsHolder from "./CardsHolder";
 import { useParams } from "react-router-dom";
 
+const index = import.meta.env.VITE_ALGOLIA_INDEX
 export default function DoctorSearch() {
   return (
-    <InstantSearch routing={true} searchClient={client} indexName="medimyth">
+    <InstantSearch routing={true} searchClient={client} indexName={index}>
       <Stack alignItems="center" width="100%" gap={4}>
         <SearchBar />
         <CardsHolder />
