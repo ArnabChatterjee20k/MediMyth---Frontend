d6957d959648e512744eea2ef9313938449e2a9e
arnab <arnabchatterjee.ac.2@gmail.com>
2023-03-12 15:27:23 +0530

    made the server address getting imported from the env

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -1,9 +1,11 @@
 export const SiteName = "MediMyth"
 export const DateFormat = 'MM/dd/yyyy'
-export const serverAddress = "http://13.127.171.39/"
+export const serverAddress = import.meta.env.VITE_BACKEND_URL
 // export const serverAddress = "http://15.207.114.250/"
 // export const serverAddress = "http://192.168.0.138:5000/"
 export const authTokenKey = "_medimythauth"
 const s3BucketName = "mmdoctorbucket"
 const regionName = "ap-south-1"
 export const s3ImageUrl = `https://${s3BucketName}.s3.${regionName}.amazonaws.com`
+
+console.log({serverAddress})
